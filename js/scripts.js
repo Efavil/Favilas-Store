302d2df9fceb8016bd76764f36bc8c534729ccd7
Eduardo <edu.piresfavila@gmail.com>
2026-04-01 20:25:30 -0300

    quase finalizando

--- js/scripts.js
+++ js/scripts.js
@@ -22,12 +22,25 @@ function update(direction) {
             active = 0
         }
     } else if(direction < 0) {
-
+        active = active -1
+        
+        if(active < 0){
+            active = total -1
+        }
     }
 
     items[active].classList.add('active')
+    dots[active].classList.add('active')
+
+    numberIndicator.textContent = String(active +1).padStart(2,'0')
+    
 
 }
+
+clearInterval(timer)
+timer = setInterval(() => {
+    update(1)
+}, 5000);
 
 prevButton.addEventListener('click', () => {
     update(-1)
